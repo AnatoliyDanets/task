fca3fb4a2ae5b900df13c1c461dee430f665ecd1
Anatoliy <78277944+AnatoliyDanets@users.noreply.github.com>
2022-04-15 23:58:13 +0300

    add svg

--- index.js
+++ index.js
@@ -34,13 +34,13 @@ function renderTable(arr) {
       <td>
       <div class="wrap-btn">
       <button type="button" class="edit"><svg class='icon-edit' version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#7f8383" width="32" height="32" viewBox="0 0 32 32">
-      <path d="M27 0c2.761 0 5 2.239 5 5 0 1.126-0.372 2.164-1 3l-2 2-7-7 2-2c0.836-0.628 1.874-1 3-1zM2 23l-2 9 9-2 18.5-18.5-7-7-18.5 18.5zM22.362 11.362l-14 14-1.724-1.724 14-14 1.724 1.724z"></path>
+      <path  d="M27 0c2.761 0 5 2.239 5 5 0 1.126-0.372 2.164-1 3l-2 2-7-7 2-2c0.836-0.628 1.874-1 3-1zM2 23l-2 9 9-2 18.5-18.5-7-7-18.5 18.5zM22.362 11.362l-14 14-1.724-1.724 14-14 1.724 1.724z"></path>
       </svg></button>
-      <button type="button" class="archiv"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#7f8383" width="32" height="32" viewBox="0 0 32 32">
+      <button type="button" class="archiv"><svg class='icon-edit' version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#7f8383" width="32" height="32" viewBox="0 0 32 32">
       <title>box-add</title>
       <path d="M26 2h-20l-6 6v21c0 0.552 0.448 1 1 1h30c0.552 0 1-0.448 1-1v-21l-6-6zM16 26l-10-8h6v-6h8v6h6l-10 8zM4.828 6l2-2h18.343l2 2h-22.343z"></path>
       </svg></button>
-      <button type="button" class="del"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#7f8383" width="32" height="32" viewBox="0 0 32 32">
+      <button type="button" class="del"><svg class='icon-edit' version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#7f8383" width="32" height="32" viewBox="0 0 32 32">
       <title>bin</title>
       <path d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
       <path d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
@@ -69,30 +69,29 @@ function submitForm(event) {
 refs.form.addEventListener("submit", submitForm);
 
 function removeNote(e) {
-  if (e.target.parentNode.classList.contains("del")) {
-    return deleteNote(e.target.parentNode.parentNode.parentNode.parentNode.id);
+  if (e.target.classList.contains("del")) {
+    return deleteNote(e.target.parentNode.parentNode.parentNode.id);
   }
 }
 
 document.addEventListener("click", removeNote);
 function openModalChange(e) {
-  if (e.target.parentNode.classList.contains("edit")) {
+  console.log();
+  if (e.target.classList.contains("edit")) {
     refs.modal1.classList.toggle("is-hidden");
-    getNoteId(e.target.parentNode.parentNode.parentNode.parentNode.id).then(
-      (data) => {
-        return Object.values(data).map((el, i, a) => {
-          return (
-            (refs.form1[0].value = a[0]),
-            (refs.form1[1].value = a[1]),
-            (refs.form1[2].value = a[2]),
-            (refs.form1[3].value = a[3]),
-            (refs.form1[4].value = a[4]),
-            refs.form1.childNodes[1].setAttribute("id", a[6]),
-            console.log(refs.form1.childNodes[1])
-          );
-        });
-      }
-    );
+    getNoteId(e.target.parentNode.parentNode.parentNode.id).then((data) => {
+      return Object.values(data).map((el, i, a) => {
+        return (
+          (refs.form1[0].value = a[0]),
+          (refs.form1[1].value = a[1]),
+          (refs.form1[2].value = a[2]),
+          (refs.form1[3].value = a[3]),
+          (refs.form1[4].value = a[4]),
+          refs.form1.childNodes[1].setAttribute("id", a[6]),
+          console.log(refs.form1.childNodes[1])
+        );
+      });
+    });
   }
 }
 document.addEventListener("click", openModalChange);
@@ -107,7 +106,8 @@ function submitChangeForm(e) {
     createDate: today.toDateString(),
     category: refs.form1[2].value,
     todo: refs.form1[3].value,
-    date: (refs.dateControl1.value = refs.dateControl1.value + " " + refs.dateControl2.value),
+    date: (refs.dateControl1.value =
+      refs.dateControl1.value + "," + refs.dateControl2.value),
     isActive: true,
   };
   const { id } = e.target.childNodes[1];
@@ -116,8 +116,8 @@ function submitChangeForm(e) {
 refs.form1.addEventListener("submit", submitChangeForm);
 
 function archivModal(e) {
-  if (e.target.parentNode.classList.contains("archiv")) {
-    const { id } = e.target.parentNode.parentNode.parentNode.parentNode;
+  if (e.target.classList.contains("archiv")) {
+    const { id } = e.target.parentNode.parentNode.parentNode;
     refs.modal3.setAttribute("id", id);
     refs.modal3.classList.toggle("is-hidden");
   }
@@ -138,7 +138,7 @@ refs.addBtnArchiv.addEventListener("click", fetchActive);
 getNotes().then((tasks) => {
   tasks.every((el) => el.isActive == true)
     ? (refs.column3.innerHTML = "<h2 style=font-size:50px;>Not Notes</h2>")
-    :refs.column3.insertAdjacentHTML("beforeend", renderTable3(tasks));
+    : refs.column3.insertAdjacentHTML("beforeend", renderTable3(tasks));
 });
 
 function renderTable3(arr) {
@@ -152,7 +152,19 @@ function renderTable3(arr) {
       <td>${el.category}</td>
       <td>${el.todo}</td>
       <td>${el.date}</td>
-      <td><button type="button" class="unarchiv">ARCH</button></td>
+      <td><button type="button" class="unarchiv"><svg
+      class="icon-edit"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#7f8383" width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      >
+      <title>box-remove</title>
+      <path
+        d="M26 2h-20l-6 6v21c0 0.552 0.448 1 1 1h30c0.552 0 1-0.448 1-1v-21l-6-6zM20 20v6h-8v-6h-6l10-8 10 8h-6zM4.828 6l2-2h18.343l2 2h-22.343z"
+      ></path>
+      </svg></button></td>
       </tr>`;
       })
       .join("");
